d9f71ffb69b5b90b898d0ef6599dc5b0c6339cc6
Paul Norman <penorman@mac.com>
2016-08-12 14:28:40 -0700

    Load ocean files into the DB

--- osm-clear.tm2source/get-shapefiles.sh
+++ osm-clear.tm2source/get-shapefiles.sh
@@ -1,41 +1,84 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e -u
 
 UNZIP_OPTS=-qqun
 
+PSQL="psql -Xq -v ON_ERROR_STOP=1"
+
+schema=shapefile_load
+
 # create and populate data dir
-mkdir -p data/simplified-water-polygons-complete-3857
-mkdir -p data/water-polygons-split-3857
+mkdir -p data/
 
+$PSQL -f - <<EOF
+DROP SCHEMA IF EXISTS $schema CASCADE;
+CREATE SCHEMA $schema;
+EOF
+
+# world_boundaries
 # simplified-water-polygons-complete-3857
+
+download="data/simplified-water-polygons-complete-3857.zip"
+table=simplified_ocean
+# Write the md5sum to a file. If the file doesn't exist, it writes a blank
+md5sum "$download" > "$download.md5" || true
+
 echo "downloading simplified-water-polygons-complete-3857..."
-curl -z "data/simplified-water-polygons-complete-3857.zip" -L -o "data/simplified-water-polygons-complete-3857.zip" "http://data.openstreetmapdata.com/simplified-water-polygons-complete-3857.zip"
-echo "simplified-water-polygons-complete-3857..."
-unzip $UNZIP_OPTS data/simplified-water-polygons-complete-3857.zip \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.shp \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.shx \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.prj \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.dbf \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.cpg \
-  -d data/
+curl -z "$download" -L -o "$download" "http://data.openstreetmapdata.com/simplified-water-polygons-complete-3857.zip"
+# Check if the file has changed
 
-# water-polygons-split-3857
-echo "downloading water-polygons-split-3857..."
-curl -z "data/water-polygons-split-3857.zip" -L -o "data/water-polygons-split-3857.zip" "http://data.openstreetmapdata.com/water-polygons-split-3857.zip"
-echo "expanding water-polygons-split-3857..."
-unzip $UNZIP_OPTS data/water-polygons-split-3857.zip \
-  water-polygons-split-3857/water_polygons.shp \
-  water-polygons-split-3857/water_polygons.shx \
-  water-polygons-split-3857/water_polygons.prj \
-  water-polygons-split-3857/water_polygons.dbf \
-  water-polygons-split-3857/water_polygons.cpg \
-  -d data/
+if md5sum --status -c "$download.md5"
+then
+  echo "$download unchanged"
+else
+  echo "Loading $download"
+  unzip $UNZIP_OPTS $download \
+    simplified-water-polygons-complete-3857/simplified_water_polygons.shp \
+    simplified-water-polygons-complete-3857/simplified_water_polygons.shx \
+    simplified-water-polygons-complete-3857/simplified_water_polygons.prj \
+    simplified-water-polygons-complete-3857/simplified_water_polygons.dbf \
+    simplified-water-polygons-complete-3857/simplified_water_polygons.cpg \
+    -d data/
+    shp2pgsql -D -I -c -g "way" -S -s 3857 data/simplified-water-polygons-complete-3857/simplified_water_polygons.shp $schema.$table \
+    | $PSQL -f -
+    $PSQL -f - <<EOF
+    SET client_min_messages = WARNING;
+    BEGIN;
+    DROP TABLE IF EXISTS public.$table;
+    ALTER TABLE $schema.$table SET SCHEMA public;
+    COMMIT;
+EOF
+fi
 
-#index
-echo "indexing shapefiles"
-shapeindex --shape_files \
-data/simplified-water-polygons-complete-3857/simplified_water_polygons.shp \
-data/water-polygons-split-3857/water_polygons.shp
+download="data/water-polygons-split-3857.zip"
+table=ocean
 
-#finish
-echo "...done!"
+# Write the md5sum to a file. If the file doesn't exist, it writes a blank
+md5sum "$download" > "$download.md5" || true
+
+echo "downloading simplified-water-polygons-complete-3857..."
+curl -z "$download" -L -o "$download" "http://data.openstreetmapdata.com/water-polygons-split-3857.zip"
+# Check if the file has changed
+
+if md5sum --status -c "$download.md5"
+then
+  echo "$download unchanged"
+else
+  echo "Loading $download"
+  unzip $UNZIP_OPTS $download \
+    water-polygons-split-3857/water_polygons.shp \
+    water-polygons-split-3857/water_polygons.shx \
+    water-polygons-split-3857/water_polygons.prj \
+    water-polygons-split-3857/water_polygons.dbf \
+    water-polygons-split-3857/water_polygons.cpg \
+    -d data/
+    shp2pgsql -D -I -c -g "way" -S -s 3857 data/water-polygons-split-3857/water_polygons.shp $schema.$table \
+    | $PSQL -f -
+    $PSQL -f - <<EOF
+    SET client_min_messages = WARNING;
+    BEGIN;
+    DROP TABLE IF EXISTS public.$table;
+    ALTER TABLE $schema.$table SET SCHEMA public;
+    COMMIT;
+EOF
+fi
